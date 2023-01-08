40d4055a842fae6360ff346078242d602615fe5d
Egorov Ivan <HD-74@yandex.ru>
2023-01-08 13:58:16 +0600

    add paths

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,12 @@
-import '../normalize.css';
+import './styles/normalize.css';
 import { FC } from 'react';
+import { MainPage, LoginPage } from '@pages';
 export const App: FC = () => {
-  return <div className='App'>APP</div>;
+  return (
+    <div className='App'>
+      APP
+      <MainPage />
+      <LoginPage />
+    </div>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@processes': path.resolve(__dirname, './src/processes'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@app': path.resolve(__dirname, './src/app'),
+    },
+  },
+});
